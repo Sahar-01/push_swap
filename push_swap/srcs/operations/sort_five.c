7aed5c80a2b3f73492e384bf17b8b348f8b060b2
Sahar <sahar@Saras-MacBook-Pro.local>
2025-08-06 17:33:58 +0100

    Solved 3/4 of sort 5

--- push_swap/srcs/operations/sort_five.c
+++ push_swap/srcs/operations/sort_five.c
@@ -1,49 +1,44 @@
 #include "../../inc/push_swap.h"
 
-int get_min_nbr(t_node *a)
+static void insert_element(t_node **a, t_node **b, t_node *overall_min, t_node *overall_max)
 {
-    int min = a->nbr;
-    while (a)
-    {
-        if (a->nbr < min)
-            min = a->nbr;
-        a = a->next;
-    }
-    return min;
-}
+    int count = 0;
+    t_node *min = get_min(*a);
+    t_node *max = get_max(*a);
 
-int get_pos(t_node *a, int val)
-{
-    int i = 0;
-    while (a)
+    if ((*b)->nbr == overall_max->nbr || (*b)->nbr > max->nbr)
     {
-        if (a->nbr == val)
-            return i;
-        i++;
-        a = a->next;
+        pa(a, b, false);
+        ra(a, false);
     }
-    return -1; // Should not happen if val is in the stack
+    else if ((*b)->nbr == overall_min->nbr || (*b)->nbr < min->nbr)
+        pa(a, b, false);
+    else
+    {
+        while ((*a)->index < (*b)->index && count < 3)
+        {
+            ra(a, false);
+            count++;
+        }
+        pa(a, b, false);
+        while (count > 0)
+        {
+            rra(a, false);
+            count--;
+        }
+    }
 }
 
 void sort_five(t_node **a, t_node **b)
 {
-    int min;
+    t_node *overall_min;
+    t_node *overall_max;
 
-    while (stack_len(*a) > 3)
-    {
-        min = get_min_nbr(*a);
-        while ((*a)->nbr != min)
-        {
-            if (get_pos(*a, min) <= stack_len(*a) / 2)
-                ra(a, false);
-            else
-                rra(a, false);
-        }
-        pb(b, a, false); // push the smallest to B
-    }
-
-    sort_three(a); // sort remaining 3 in A
-
-    while (*b)
-        pa(a, b, false); // push back from B to A
+    overall_min = get_min(*a);
+    overall_max = get_max(*a);
+    pb(b, a, false);
+    pb(b, a, false);
+    sort_three(a);
+    insert_element(a, b, overall_min, overall_max);
+    insert_element(a, b, overall_min, overall_max);
 }
