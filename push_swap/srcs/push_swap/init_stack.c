6d9266067d8d346c6d92e7a2be8b3fcff3eeddd2
Sahar <sahar@Saras-MacBook-Pro.local>
2025-08-07 23:15:29 +0100

    Tried a minor fix of memory leaks

--- push_swap/srcs/push_swap/init_stack.c
+++ push_swap/srcs/push_swap/init_stack.c
@@ -1,6 +1,6 @@
 #include "../../inc/push_swap.h"
 
-static void ps_error(char **arr)
+static void ps_error(char **arr, t_node **a)
 {
     int i = 0;
     if (arr)
@@ -9,11 +9,21 @@ static void ps_error(char **arr)
             free(arr[i++]);
         free(arr);
     }
+    if (a)
+    {
+        t_node *tmp;
+        while (*a)
+        {
+            tmp = (*a)->next;
+            free(*a);
+            *a = tmp;
+        }
+    }
     write(2, "Error\n", 6);
     exit(EXIT_FAILURE);
 }
 
-static long ps_atol(const char *s)
+static long ps_atol(const char *s, t_node **a)
 {
     long res = 0;
     int sign = 1;
@@ -27,16 +37,16 @@ static long ps_atol(const char *s)
         s++;
     }
     if (!*s)
-        ps_error(NULL);
+        ps_error(NULL, a);
     while (*s && ft_isdigit(*s))
     {
         res = res * 10 + (*s - '0');
         if ((sign == 1 && res > INT_MAX) || (sign == -1 && -res < INT_MIN))
-            ps_error(NULL);
+            ps_error(NULL, a);
         s++;
     }
     if (*s != '\0')
-        ps_error(NULL);
+        ps_error(NULL, a);
     return (res * sign);
 }
 
@@ -46,7 +56,7 @@ static void validate_and_push(t_node **a, long val)
     while (tmp)
     {
         if (tmp->nbr == (int)val)
-            ps_error(NULL);
+            ps_error(NULL, a);
         tmp = tmp->next;
     }
     ps_lstadd_back(a, ps_lstnew((int)val));
@@ -77,11 +87,11 @@ void init_stack(t_node **a, char **argv)
     {
         char **split = ft_split(argv[i], ' ');
         if (!split || !*split)
-            ps_error(split);
+            ps_error(split, a);
         int j = 0;
         while (split[j])
         {
-            long val = ps_atol(split[j]);
+            long val = ps_atol(split[j], a);
             validate_and_push(a, val);
             free(split[j]);
             j++;
